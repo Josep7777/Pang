43d7b032678eb12460d1de16772db7a664a22be3
Josep <josep.romera@enti.cat>
2022-11-09 23:49:55 +0100

    Muros,paredes y HUD basico

--- js/pang.js
+++ js/pang.js
@@ -8,7 +8,9 @@ class pang extends Phaser.Scene {
     //this.load.image('background','Background.png');
     this.load.image("harpoon", "Harpoon0.png");
     this.load.image("ball", "ball1.png");
-    this.load.image("floor", "Floor.png");
+    this.load.image("floor", "Floor2.png");
+    this.load.image("wall", "Wall.png");
+    this.load.image("lifes","lifes.png");
     this.load.image("pu_double_wire", "PowerUpDoubleWire.png");
     this.load.spritesheet("player1", "Character.png", {
       frameWidth: 31,
@@ -38,6 +40,7 @@ class pang extends Phaser.Scene {
     this.powerUpWire = this.physics.add.sprite(config.width/2, config.height/2,'pu_double_wire').setScale(3);
     this.harpoonNumber = 0; //Variable que se usara para determinar cuantos disparos consecutivos puede hacer el jugador
     this.harpoonNumberMax = 1;
+    this.score = 0;
     //Añadimos al jugador con fisicas
     this.player1 = this.physics.add
       .sprite(config.width / 2, config.height - 250, "player1")
@@ -45,18 +48,31 @@ class pang extends Phaser.Scene {
       .setFrame(4);
     
     this.player1Health = 3;
-
+    //Datos del HUD
+    this.levelName = "MT.FUJI";
+    this.worldNumber = 1;
+    this.stageNumber = 1;
+    this.highScore = 100000;
+    this.timer = 99;
     //Creamos los cursores para input
     this.cursores = this.input.keyboard.createCursorKeys();
 
-    //Creamos el suelo
-    this.floor = this.physics.add.sprite(550, config.height - 190, "floor");
-    this.floor.body.allowGravity = false;
-    this.floor.body.setImmovable(true);
+    //Funcion que crea los textos estaticos
+    this.loadText();
+    //Crea los muros y paredes
+    this.createWalls();
 
+    //HUD de las vidas
+    this.live1 = this.add.sprite(config.width - 1750, config.height - 50, "lifes").setScale(4);
+    this.live2 = this.add.sprite(config.width - 1690, config.height - 50, "lifes").setScale(4);
+    this.live3 = this.add.sprite(config.width - 1630, config.height - 50, "lifes").setScale(4);
     //Añadimos colisiones
-    this.physics.add.collider(this.floor, this.player1);
-    this.physics.add.collider(this.floor, this.ballpool);
+    this.physics.add.collider(this.floorD, this.player1);
+    this.physics.add.collider(this.wall, this.player1);
+    this.physics.add.collider(this.wallR, this.player1);
+    this.physics.add.collider(this.floorD, this.ballpool);
+    this.physics.add.collider(this.wall,  this.ballpool);
+
     //this.floor2 = this.add.sprite(1400,890,'floor');
     //this.floor = this.physics.add.sprite(180,100,'floor');
 
@@ -216,6 +232,7 @@ class pang extends Phaser.Scene {
   }
 
   hitBall(_harpoon, _ballCol) {
+    this.score += 10;
     if (_ballCol.scale > 1) {
       this.createBall(_ballCol.x + 30 * _ballCol.scale * 2, _ballCol.y, _ballCol.scale - 1)
       this.createBall(_ballCol.x - 30 * _ballCol.scale * 2, _ballCol.y, _ballCol.scale - 1)
@@ -270,6 +287,91 @@ class pang extends Phaser.Scene {
     _powerUp.body.setVelocityY(gamePrefs.POWER_UP_SPEED);
   }
 
+  loadText(){
+    //PLAYERS
+    this.add.text
+		(250, 750,'PLAYER-1',{
+				fontFamily: 'Public Pixel',
+				fill: '#FFFFFF',
+				stroke:'#FFFFFF',
+			}
+		).setOrigin(.5).setScale(2);
+
+    this.add.text
+		(1550, 750,'PLAYER-2',{
+				fontFamily: 'Public Pixel',
+				fill: '#FFFFFF',
+				stroke:'#FFFFFF',
+			}
+		).setOrigin(.5).setScale(2);
+    //NOMBRE DEL MUNDO ACTUAL
+    this.add.text
+		(1000, 750,this.levelName,{
+				fontFamily: 'Public Pixel',
+				fill: '#FFFFFF',
+				stroke:'#FFFFFF',
+			}
+		).setOrigin(.5).setScale(2);
+    //NUMERO DE MUNDO Y NIVEL
+    this.add.text
+		(1000, 830,this.worldNumber + "-" + this.stageNumber + " STAGE",{
+				fontFamily: 'Public Pixel',
+				fill: '#FFFFFF',
+				stroke:'#FFFFFF',
+			}
+		).setOrigin(.5).setScale(2);
+    //HIGH SCORE
+    this.add.text
+		(1000, 870,"HI: "+ this.highScore,{
+				fontFamily: 'Public Pixel',
+				fill: '#FFFFFF',
+				stroke:'#FFFFFF',
+			}
+		).setOrigin(.5).setScale(2);
+    //INSERT COIN
+    this.add.text
+		(1550, 830,"INSERT COIN",{
+				fontFamily: 'Public Pixel',
+				fill: '#FFFFFF',
+				stroke:'#FFFFFF',
+			}
+		).setOrigin(.5).setScale(2);
+    
+  }
+  updateText(){
+    //SCORE
+    this.add.text
+		(400, 800,this.score,{
+				fontFamily: 'Public Pixel',
+				fill: '#FFFFFF',
+				stroke:'#FFFFFF',
+			}
+		).setOrigin(.5).setScale(2);
+    //TIMER
+    this.add.text
+		(1600, 100,"TIME:0"+this.timer,{
+				fontFamily: 'Public Pixel',
+				fill: '#FFFFFF',
+				stroke:'#FFFFFF',
+			}
+		).setOrigin(.5).setScale(4);
+  }
+  createWalls(){
+    //Creamos el suelo
+    this.floorD = this.physics.add.sprite(config.width - 960, config.height - 185, "floor");
+    this.floorD.body.allowGravity = false;
+    this.floorD.body.setImmovable(true);
+    this.floorU = this.physics.add.sprite(config.width - 960, config.height - 882, "floor");
+    this.floorU.body.allowGravity = false;
+    this.floorU.body.setImmovable(true);
+    //Creamos las paredes
+    this.wall = this.physics.add.sprite(20, config.height - 543, "wall");
+    this.wall.body.allowGravity = false;
+    this.wall.body.setImmovable(true);
+    this.wallR = this.physics.add.sprite(config.width - 20, config.height - 543, "wall");
+    this.wallR.body.allowGravity = false;
+    this.wallR.body.setImmovable(true);
+  }
   loadAnimations() {
     this.anims.create({
       key: "shoot",
@@ -285,7 +387,19 @@ class pang extends Phaser.Scene {
       repeat: -1,
     });
   }
-
+  lifesHUD(){
+    //Se van eliminando el indicador de vidas conforme se va perdiendo
+    if(this.player1Health == 2){
+      this.live3.destroy();
+    }
+    else if(this.player1Health == 1){
+      this.live2.destroy();
+    }
+    else if(this.player1Health == 0){
+      this.live1.destroy();
+      console.log("GAME OVER");
+    }
+  }
   update() {
     if (this.cursores.left.isDown) {
       this.player1.setFlipX(false);
@@ -299,5 +413,9 @@ class pang extends Phaser.Scene {
       this.player1.body.setVelocityX(0);
       if (!this.isShooting) this.player1.setFrame(4);
     }
+    //Textos que cambian segun avanza la partida
+    this.updateText();
+    //Funcion que controla el HUD de las vidas
+    this.lifesHUD();
   }
 }
