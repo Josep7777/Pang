c19ba612d4bb8311bc34b435d36a0f70cba04f73
Ivan Sales <ivan.sales@enti.cat>
2022-12-12 22:19:12 +0100

    Empezado el tema de la comida

---
+++ js/prefabs/foodPrefab.js
@@ -0,0 +1,44 @@
+class foodPrefab extends Phaser.GameObjects.Sprite {
+  constructor(scene, positionX, positionY, _tag) {
+    super(scene, positionX, positionY, _tag);
+    scene.add.existing(this);
+
+    scene.physics.add.overlap(
+      scene.player1,
+      this,
+      this.getPoints,
+      null,
+      this
+    );
+
+    this.powerUpTimer = scene.time.addEvent({
+      delay: gamePrefs.POWERUP_DESTROY_TIMER, //ms
+      callback: this.destroyFood,
+      callbackScope: this,
+      repeat: 0,
+    });
+
+    this.scene = scene;
+  }
+
+  destroyFood(){
+    var i = 0;
+    this.tintTimer = this.scene.time.addEvent({
+      delay: 200,
+      callback: () => {
+        if (i % 2 == 0) this.tint = 0xffffff;
+        else this.tint = 0x9b9b9b;
+
+        i++;
+        if (i >= 20) this.destroy();
+      },
+      callbackScope: this,
+
+      repeat: 20,
+    });
+  }
+
+  getPoints(_player, _food){
+    this.scene._hud.setScore(500);
+  }
+}
