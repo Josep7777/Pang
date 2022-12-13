36fb17fd009f5027e3cf14343dd10eaec0e1bc5c
Ivan Sales <ivan.sales@enti.cat>
2022-12-13 10:54:01 +0100

    Organizado imagenes y añadidas todas las comidas

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -4,16 +4,18 @@ class mainMenu extends Phaser.Scene {
   }
   preload() {
     /***********************Sprites/Imagenes**********************/
-    this.load.setPath("assets/img/");
+    //this.load.setPath("assets/img/");
 
-    //Jugador
+    //Character
+    this.load.setPath("assets/img/character");
     this.load.spritesheet("player1", "Character.png", {
       frameWidth: 31,
       frameHeight: 32,
     });
     this.load.image("harpoon", "Harpoon0.png");
 
-    //Plataformas
+    //Platforms
+    this.load.setPath("assets/img/platforms");
     this.load.image("floor", "Floor2.png");
     this.load.image("wall", "Wall.png");
     this.load.image("normalPlat", "PlataformaRoja1.png");
@@ -26,9 +28,8 @@ class mainMenu extends Phaser.Scene {
     this.load.image("normalBlueHorizontal", "PlataformaAzulClaroV.png");
     this.load.image("ladder", "escalera.png");
 
-    //Fondos
-    this.load.image("winImage", "winImage1.png");
-    this.load.image("map", "map.png");
+    //Levels
+    this.load.setPath("assets/img/levels");
     this.load.image("background", "Background.png");
     this.load.image("background2", "background1-2.png");
     this.load.image("background3", "background1-3.png");
@@ -37,17 +38,21 @@ class mainMenu extends Phaser.Scene {
     this.load.image("background6", "background1-6.png");
     this.load.image("background7", "background1-7.png");
     this.load.image("background8", "background1-8.png");
+
+    //HUD
+    this.load.setPath("assets/img/hud");
+    this.load.image("lifes", "lifes.png");
+    this.load.image("markYellow", "markYellow.png");
+    this.load.image("markOrange", "markOrange.png");
+    this.load.image("map", "map.png");
     this.load.image("logo", "pangLogo.gif");
+    this.load.image("winImage", "winImage1.png");
+    this.load.image("plane", "plane.png");
 
     //PowerUps
+    this.load.setPath("assets/img/powerUps");
     this.load.image("powerUp1", "PowerUpDoubleWire.png");
     this.load.image("powerUp2", "powerUpEscudo.png");
-    this.load.spritesheet("escudo", "Shield.png", {
-      frameWidth: 37,
-      frameHeight: 41,
-    });
-
-    this.load.image("powerUp3", "fresa.png");
     this.load.image("powerUp4", "PowerUpStopTime.png");
     this.load.image("powerUp5", "powerUpGanchoFijo.png");
     this.load.spritesheet("powerUp6", "PowerUpDinamita.png", {
@@ -56,17 +61,61 @@ class mainMenu extends Phaser.Scene {
     });
     this.load.image("powerUp7", "PowerUpSlowTime.png");
     this.load.image("powerUp8", "powerUpDoubleShoot.png");
-    this.load.image("plane", "plane.png");
     this.load.spritesheet("doubleShoot", "doubleShoot.png", {
       frameWidth: 19,
       frameHeight: 9,
     });
-    //Enemigos
-    this.load.image("ball", "ball1.png");
+
+    //Particles
+    this.load.setPath("assets/img/particles");
     this.load.spritesheet("ballExplosion", "BallExplosion.png", {
       frameWidth: 62,
       frameHeight: 52,
     });
+
+    this.load.spritesheet("enemyDeath", "EnemiesDeath.png", {
+      frameWidth: 33,
+      frameHeight: 32,
+    });
+
+    this.load.spritesheet("escudo", "Shield.png", {
+      frameWidth: 37,
+      frameHeight: 41,
+    });
+
+    //Comidas
+    this.load.setPath("assets/img/food");
+    this.load.image("food1", "berenjena.png");
+    this.load.image("food2", "calabaza.png");
+    this.load.image("food3", "cereza.png");
+    this.load.image("food4", "chirimoya.png");
+    this.load.image("food5", "donut.png");
+    this.load.image("food6", "hamburguesa.png");
+    this.load.image("food7", "judia.png");
+    this.load.image("food8", "limon.png");
+    this.load.image("food9", "maiz.png");
+    this.load.image("food10", "manzana.png");
+    this.load.image("food11", "melon.png");
+    this.load.image("food12", "naranja.png");
+    this.load.image("food13", "pastel.png");
+    this.load.image("food14", "patatas.png");
+    this.load.image("food15", "pepino.png");
+    this.load.image("food16", "perrito.png");
+    this.load.image("food17", "pimiento.png");
+    this.load.image("food18", "piña.png");
+    this.load.image("food19", "platano.png");
+    this.load.image("food20", "pollo.png");
+    this.load.image("food21", "salchicha.png");
+    this.load.image("food22", "sandia.png");
+    this.load.image("food23", "seta.png");
+    this.load.image("food24", "sushi.png");
+    this.load.image("food25", "tomate.png");
+    this.load.image("food26", "uva.png");
+    this.load.image("food27", "zanahoria.png");
+    this.load.image("powerUp3", "fresa.png");
+
+    //Enemigos
+    this.load.setPath("assets/img/enemies");
     this.load.spritesheet("crab", "crab.png", {
       frameWidth: 38.4,
       frameHeight: 30,
@@ -74,10 +123,6 @@ class mainMenu extends Phaser.Scene {
     this.load.spritesheet("bird", "birdEnemy.png", {
       frameWidth: 34,
       frameHeight: 24,
-    });
-    this.load.spritesheet("enemyDeath", "EnemiesDeath.png", {
-      frameWidth: 33,
-      frameHeight: 32,
     });
     this.load.spritesheet("owl", "owl.png", {
       frameWidth: 38.36,
@@ -88,10 +133,9 @@ class mainMenu extends Phaser.Scene {
       frameHeight: 26,
     });
 
-    //UI
-    this.load.image("lifes", "lifes.png");
-    this.load.image("markYellow", "markYellow.png");
-    this.load.image("markOrange", "markOrange.png");
+    //Balls
+    this.load.setPath("assets/img/balls");
+    this.load.image("ball", "ball1.png");
 
     /************************Sonidos***********************/
     this.load.setPath("assets/music/");
